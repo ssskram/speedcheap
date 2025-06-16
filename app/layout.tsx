e28c2385ca00791e693ff318ada8cfeafbd6eaa1
ssskram <paulmarks211@gmail.com>
2025-06-15 22:36:42 -0700

    Add favicon and truck images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,32 @@
-import type { Metadata } from "next";
+import { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
-  title: "Speedcheap",
+  title: "Speed is cheap",
   description: "The faster we move, the less it's all worth",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+type RootLayoutProps = {
   children: React.ReactNode;
-}>) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
+      <head>
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          sizes="16x16"
+          type="image/x-icon"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
