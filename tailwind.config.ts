0059fb51ca938f5bc1c74f61020b39bf61570316
ssskram <paulmarks211@gmail.com>
2025-06-19 21:51:11 -0700

    GUTTED

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './hooks/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'shake': 'shake 0.5s cubic-bezier(.36,.07,.19,.97) both',
+      },
+      keyframes: {
+        shake: {
+          '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
+          '20%, 80%': { transform: 'translate3d(2px, 0, 0)' },
+          '30%, 50%, 70%': { transform: 'translate3d(-4px, 0, 0)' },
+          '40%, 60%': { transform: 'translate3d(4px, 0, 0)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config; 
